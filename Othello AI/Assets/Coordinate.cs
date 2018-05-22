d9d1335eeeb6f9628bea4d1f677fcd5ea18d1bd7
hecris <hecargMAL@GMAIL.COM>
2018-05-22 11:40:03 -0400

    functional Othello board with simple AI

---
+++ Othello AI/Assets/Coordinate.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coordinate {
+
+    private int x = -1;
+    private int y = -1;
+
+    public Coordinate()  {}
+
+    public Coordinate(int x, int y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+
+
+
+
+
+    public int getX()
+    {
+        return x;
+    }
+
+    public int getY()
+    {
+        return y;
+    }
+
+    public string AsString()
+    {
+        return x.ToString() + "," + y.ToString();
+    }
+}
